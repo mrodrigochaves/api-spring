38c4e679e940ffea568cba3704cc3854fb4445d2
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-04-11 21:03:39 -0300

    update post function

--- src/main/java/com/mrodrigochaves/apispring/controller/CarsController.java
+++ src/main/java/com/mrodrigochaves/apispring/controller/CarsController.java
@@ -2,8 +2,14 @@ package com.mrodrigochaves.apispring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mrodrigochaves.apispring.model.Car;
@@ -21,10 +27,18 @@ public class CarsController {
     private CarsRepository carsRepository;
 
 
-
+    //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
-    public List<Car> list(){
+    public @ResponseBody List<Car> list(){
         return carsRepository.findAll();
+    }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED )
+    public Car create(@RequestBody Car car ) {
+        return carsRepository.save(car);
+              
     }
     
 }
