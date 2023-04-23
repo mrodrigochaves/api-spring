92d602fdfbf8e8afe30e42a6de792fa907e2e741
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-04-22 21:49:37 -0300

    Add second car

--- src/main/java/com/mrodrigochaves/apispring/controller/CarsController.java
+++ src/main/java/com/mrodrigochaves/apispring/controller/CarsController.java
@@ -3,8 +3,9 @@ package com.mrodrigochaves.apispring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,13 @@ public class CarsController {
     public @ResponseBody List<Car> list(){
         return carsRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> findById(@PathVariable Long id){
+        return carsRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
+    } 
 
     //@RequestMapping(method = RequestMethod.POST)
     @PostMapping
